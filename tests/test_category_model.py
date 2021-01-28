422698567efe3308e7007e1a750bd4a8468e00d6
marcosdcl <marcosdouglasclima@gmail.com>
2021-01-28 15:12:21 -0300

    Create test for category model name type

--- tests/test_category_model.py
+++ tests/test_category_model.py
@@ -12,3 +12,10 @@ def test_category_model_contructor():
     category = Category('Test Name', 'Test Desciption')
     assert category.name == 'Test Name'
     assert category.description == 'Test Desciption'
+
+def test_category_model_name_exception_if_type_not_string():
+    try:
+        category = Category(None, 'description')
+        raise NotImplementedError('The expected exception was not raised.')
+    except Exception as error:
+        assert isinstance(error, TypeError)
