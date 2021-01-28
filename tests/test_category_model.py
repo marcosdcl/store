a7bfe981814bfc5bad523be44d9fdcb122129d72
marcosdcl <marcosdouglasclima@gmail.com>
2021-01-28 14:43:57 -0300

    Create test for category model contructor

--- tests/test_category_model.py
+++ tests/test_category_model.py
@@ -1,6 +1,14 @@
+import sys
+sys.path.append('')
+
 from models.categorty_model import Category
 
 
-def test_model_instance():
+def test_category_model_instance():
     category = Category('Test Name', 'Test Desciption')
     assert isinstance(category, Category)
+
+def test_category_model_contructor():
+    category = Category('Test Name', 'Test Desciption')
+    assert category.name == 'Test Name'
+    assert category.description == 'Test Desciption'
