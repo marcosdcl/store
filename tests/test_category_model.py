c40679b9b162502768248f40f8581a248ac26eaa
ThiagoFiorelli <36682785+ThiagoFiorelli@users.noreply.github.com>
2021-01-29 16:53:08 -0300

    Pytest tools added to test functions and updated requirements file

--- tests/test_category_model.py
+++ tests/test_category_model.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('')
 
 from models.categorty_model import Category
+import pytest
 
 
 def test_category_model_instance():
@@ -9,34 +10,39 @@ def test_category_model_instance():
     assert isinstance(category, Category)
 
 def test_category_model_contructor():
-    category = Category('Test Name', 'Test Desciption')
-    assert category.name == 'Test Name'
-    assert category.description == 'Test Desciption'
+    name = 'Test Name'
+    description = 'Test Desciption'
+    category = Category(name, description)
+    assert category.name is name
+    assert category.description is description
 
-def test_category_model_name_exception_if_type_not_string():
-    try:
-        category = Category(None, 'description')
-        raise NotImplementedError('The expected exception was not raised.')
-    except Exception as error:
-        assert isinstance(error, TypeError)
+@pytest.mark.parametrize("name, description", [
+    (10, 'Desc Categoria'),
+    (10.5, 'Desc Categoria'),
+    (False, 'Desc Categoria')
+])
+def test_category_model_name_not_str_exception(name, description):
+    with pytest.raises(TypeError):
+        category = Category(name, description)
 
-def test_category_model_name_exception_if_string_is_empty():
-    try:
+@pytest.mark.parametrize("name, description", [
+    ('Desc nome', None),
+    ('Desc nome', 10.5),
+    ('Desc nome', False)
+])
+def test_category_model_description_not_str_exception(name, description):
+    with pytest.raises(TypeError):
+        category = Category(name, description)
+
+def test_category_model_name_empty_exception():
+    with pytest.raises(ValueError):
         category = Category('   ', 'description')
-        raise NotImplementedError('The expected exception was not raised.')
-    except Exception as error:
-        assert isinstance(error, ValueError)
 
-def test_category_model_name_exception_if_string_is_bigger_than_100_characters():
-    try:
+def test_category_model_name_bigger_than_100_characters_exception():
+    with pytest.raises(ValueError):
         category = Category('i'*101, 'description')
-        raise NotImplementedError('The expected exception was not raised.')
-    except Exception as error:
-        assert isinstance(error, ValueError)
 
-def test_category_model_description_exception_if_string_is_bigger_than_255_characters():
-    try:
+def test_category_model_description_bigger_than_255_characters_exception():
+    with pytest.raises(ValueError):
         category = Category('name', 'i'*256)
-        raise NotImplementedError('The expected exception was not raised.')
-    except Exception as error:
-        assert isinstance(error, ValueError)
+
